1258753f10336eae7969168d22d1278de6f08e8d
Abdelrady <ahmedmc95@gmail.com.com>
2024-05-19 10:13:58 +0300

    Next Portfolio

---
+++ data/index.ts
@@ -0,0 +1,216 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Full-Stack Todo Next.Js",
+    des: "Explore the wonders of Todo.",
+    img: "/Full-Stack Todo App.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg","/prisma.png","/mongodb.svg"],
+    link: "https://fullstack-nextjs-todo-app-teal.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Walmart - E-commerce App",
+    des: "Simplify your Products",
+    img: "/Walmart _ Save Money.Live better.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png","/node-js.svg","/mongodb.svg"],
+    link: "https://graduation-project-walmart-clone.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Shopify E-commerce",
+    des: "A REAL Software-as-a-Service app using vanilla javascript",
+    img: "/VainllaJs.png",
+    iconLists: ["/html5.png", "/css.png", "/js.png"],
+    link: "https://shopify-two-wine.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Product Builder",
+    des: "Product Builder Power of typescript",
+    img: "/Product Builder.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",],
+    link: "https://product-builder-swart.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus modi alias excepturi iste dignissimos nostrum veritatis vitae, in totam dolores?",
+    name: "Ahmed Ali",
+    title: "Software Engineer",
+  },
+  {
+    quote:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus modi alias excepturi iste dignissimos nostrum veritatis vitae, in totam dolores?",
+    name: "Mohamed",
+    title: "Software Engineer",
+  },
+  {
+    quote:
+    "شخصية مهنيه محترمه اوصي بالتعامل معه",
+    name: "حافظ على",
+    title: "مدير مشاريع",
+  },
+  {
+    quote:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus modi alias excepturi iste dignissimos nostrum veritatis vitae, in totam dolores?",
+    name: "Michael Johnson",
+    title: "Software Engineer",
+  },
+  {
+    quote:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus modi alias excepturi iste dignissimos nostrum veritatis vitae, in totam dolores?",
+    name: "Omer",
+    title: "Software Engineer",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "amazon",
+    img: "/Amazon_PNG6.png",
+    nameImg: "/Amazon_PNG6.png",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Fullstack Engineer Intern ITI",
+    desc: "Assisted in the development of a web-based platform using MongoDB, Express, Angular, React.js, Node.Js ,enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Fullstack MERN",
+    desc: "Designed and developed Web app using React.Js",
+    className: "md:col-span-2", 
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a Web app for a client, from initial concept to deployment on Hosts",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Abdelrady-M",
+  },
+ 
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/abdelrady-mohamed/",
+  },
+];
